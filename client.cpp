1136a43157918476ea6ae10c55a0a150e1f29bd5
Shivani Dwivedi <princi6124@gmail.com>
2025-08-29 20:00:44 +0530

    Multiple bytes msg can be sent now by the client

--- client.cpp
+++ client.cpp
@@ -5,14 +5,92 @@
 #include <cstdlib>  //for abort
 #include <cstring>  //for strerror
 #include <unistd.h> // for write
+#include <assert.h> //for assert
 
 using namespace std;
 
+static void msg(string str)
+{
+    cerr << str << endl;
+}
 void die(string msg)
 {
     int err = errno;
     cerr << "[" << err << "] " << msg << " (" << strerror(err) << ")" << endl;
     abort();
+}
+
+static int read_full(int fd, char *rbuff, u_int32_t n)
+{
+    while (n > 0)
+    {
+        u_int32_t rv = read(fd, rbuff, n);
+        if (rv == 0)
+        {
+            // msg("EOF");
+            return -1;
+        }
+        else if (rv < 0)
+        {
+            msg("read() error ");
+            return -1;
+        }
+        assert(rv <= n);
+        n -= rv;
+        rbuff += rv;
+    }
+    return 0;
+}
+
+static int write_all(int fd, char *w_buff, u_int32_t n)
+{
+    while (n > 0)
+    {
+        int rv = write(fd, w_buff, n);
+        if (rv <= 0)
+            return -1;
+        assert(rv <= n);
+        n -= rv;
+        w_buff += rv;
+    }
+    return 0;
+}
+
+const u_int32_t k_max_size = 4096;
+
+static int query(int fd, const char *text)
+{
+    u_int32_t len = (u_int32_t)strlen(text);
+    if (len > k_max_size)
+        return -1;
+    char w_buff[4 + len];
+    memcpy(w_buff, &len, 4);
+    memcpy(w_buff + 4, text, len);
+    int err = write_all(fd, w_buff, len + 4);
+    if (err)
+        return err;
+
+    char rbuff[4 + k_max_size];
+    errno = 0;
+    err = read_full(fd, rbuff, 4);
+    if (err != 0)
+    {
+        msg("read() error ");
+        return -1;
+    }
+
+    memcpy(&len, rbuff, 4);
+    if (len > k_max_size)
+        msg("too long ");
+    err = read_full(fd, rbuff + 4, len);
+    if (err)
+        msg("read() error ");
+
+    // printing msg stored
+
+    string message(rbuff + 4, len);
+    cout << "Server says " << message << endl;
+    return 0;
 }
 int main()
 {
@@ -27,21 +105,27 @@ int main()
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK);
     addr.sin_port = ntohs(1234);
-    int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
 
     // Establishing the Connection
 
-    rv = connect(fd, (const sockaddr *)&addr, sizeof(addr));
+    int rv = connect(fd, (const sockaddr *)&addr, sizeof(addr));
+    if (rv)
+        die("connect() ");
 
-    // Read and Write test
+    // Read and Write multiple requests for test
 
-    string msg = "hello";
-    write(fd, msg.c_str(), msg.size());
-    char rbuff[64] = {};
-    ssize_t n = read(fd, rbuff, sizeof(rbuff) - 1);
-    if (n < 0)
-        die("read()");
-    cout << "Server says " << rbuff << endl;
+    int err = query(fd, "Hello this is Shivani");
+    if (err)
+    {
+        close(fd);
+        return 0;
+    }
+    err = query(fd, "How are you");
+    if (err)
+    {
+        close(fd);
+        return 0;
+    }
     close(fd);
     return 0;
 }
